3407112865dbc49e70312735cd87fc8857a7db7d
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-10-30 16:27:27 -0300

    feat: adicionado metodo para transformar o conteudo das variaveis em string

--- Trabalho 01/entity/Position.py
+++ Trabalho 01/entity/Position.py
@@ -2,19 +2,19 @@ class Position():
     def __init__(self, line: int, column: int):
         self._line = line
         self._column = column
-    
+
     @property
     def line(self) -> int:
         return self._line
-    
+
     @property
     def column(self) -> int:
         return self._column
-    
+
     @line.setter
     def line(self, line: int):
         self._line = line
-    
+
     @column.setter
     def column(self, column: int):
         self._column = column
@@ -23,4 +23,5 @@ class Position():
         self._line = line
         self._column = column
 
-
+    def __str__(self) -> str:
+        return f"line: {self.line}, column: {self.column}"
