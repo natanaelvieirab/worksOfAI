d52c3a6660c621bb52ccc1e177d98b1d2c426c90
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-11-01 20:50:43 -0300

    fix: corrigido estrutura da lista de matrizes
    ao rodar a aplicacao , estava sendo execultada a variável data, juntamente com cada dicionario do tipo board, o que acarretava em uso desnecessáro de memória

--- Trabalho 01/tests/data.py
+++ Trabalho 01/tests/data.py
@@ -1,41 +1,40 @@
-from entity.Game import Game
 
 data = [
     {
         "isSolvable": True,
-        "board": Game([
+        "board": [
             [12, 1, 10, 2],
             [7, 11, 4, 14],
             [5, 0, 9, 15],
             [8, 13, 6, 3],
-        ]),
+        ],
     },
     {
         "isSolvable": True,
-        "board": Game([
+        "board":  [
             [13, 2, 10, 3],
             [1, 12, 8, 4],
             [5, 0, 9, 6],
             [15, 14, 11, 7],
-        ]),
+        ],
     },
     {
         "isSolvable": True,
-        "board": Game([
+        "board":  [
             [6, 13, 7, 10],
             [8, 9, 11, 0],
             [15, 2, 12, 5],
             [14, 3, 1, 4],
-        ]),
+        ],
     },
     {
         "isSolvable": False,
-        "board": Game([
+        "board":  [
             [3, 9, 1, 15],
             [14, 11, 4, 6],
             [13, 0, 10, 12],
             [2, 7, 8, 5],
-        ]),
+        ],
     },
 ]
 
@@ -43,18 +42,22 @@ data = [
 requiredData = [
     {
         "isSolvable": True,
-        "board": Game([
+        "board": [
             [1, 2, 3, 4],
             [5, 6, 8, 12],
             [13, 9, 0, 7],
             [14, 11, 10, 15]
-        ]),
+        ],
+    }, {
         "isSolvable": False,
-        "board": Game([
+        "board": [
             [1, 2, 3, 4],
             [13, 6, 8, 12],
             [5, 9, 0, 7],
             [14, 11, 10, 15]
-        ]),
+        ],
     },
 ]
+
+if __name__ == "__main__":
+    print(requiredData[0]["board"])
