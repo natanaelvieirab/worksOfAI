8a6972019bcfe43dbef10d06cda70169a4632c2e
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-11-13 15:18:32 -0300

    refactor: removendo comentarios

--- Trabalho 02/utils/Operations.py
+++ Trabalho 02/utils/Operations.py
@@ -41,13 +41,9 @@ class Operations:
 
         firstMostSignificant = heappop(heap)[1]
         children = []
-        # print(
-        #     f"mais significativo:{firstMostSignificant}, em decimal:{self.__toDecimal(firstMostSignificant)}\n")
 
         for i in range(0, self.sizePopulation//2):
             relevantIndividual = heappop(heap)[1]
-            # print(
-            #     f"value:{relevantIndividual}, em decimal: {self.__toDecimal(relevantIndividual)}")
 
             aux = self.__crossover([firstMostSignificant, relevantIndividual])
 
@@ -57,61 +53,50 @@ class Operations:
         return children
 
     def __crossover(self, identifications) -> list:
-        # ao extrair tem que levar em consideração:
-        # [x] se tem sinal , pq conta como caractere;
-        # [x] tamanho da string
-
-        # ao juntar, levar em consideração o sinal ?
         piece = []
         children = []
 
         interation = 1
+        isNegative = False
+
         for identification in identifications:
             if '-' in identification:
-                # print(f"O numero {identification} é negativo")
                 isNegative = True
                 identification = identification.split('-')[1]
 
             pos = self.__getPositionCrossover(identification)
             length = len(identification)
 
-            if pos == length:
-                length += 1
+            if length == 1:
+                genes1 = genes2 = identification
+            else:
+                genes1 = identification[pos: length] if interation == 1 else identification[0: pos]
+                genes2 = identification[0: pos] if interation == 1 else identification[pos: length]
 
-            if pos == 0 and length == 1:
-                pos += 1
-
-            genes1 = identification[pos: length] if interation == 1 else identification[0: pos]
-            genes2 = identification[0: pos] if interation == 1 else identification[pos: length]
-
-            # print(
-            #     f"identificador{interation}: {identification}\tindice:{pos}\tlen:{length}\textraido:{genes1} e {genes2}")
             piece.append(genes1)
             piece.append(genes2)
             interation += 1
 
         """Embaralhando genes"""
-        individualGenerate = piece[0]+piece[3]
-        individualGenerate2 = piece[2]+piece[1]
-
-        print(f"resultado da junção de 0 e 3 : {individualGenerate}\n")
-        print(f"resultado da junção de 2 e 1 : {individualGenerate2}\n")
-
-        children.append(Individual(individualGenerate,
-                                   self.__toDecimal(individualGenerate)))
-        children.append(Individual(individualGenerate2,
-                                   self.__toDecimal(individualGenerate2)))
+        children.append(self.__join(piece[0], piece[3]))
+        children.append(self.__join(piece[2], piece[1]))
 
         return children
 
     def __getPositionCrossover(self, identification):
         return (self.percentageCrossover * len(identification)) // 100
 
+    def __join(self, genes1, genes2):
+        individualGenerate = genes1 + genes2
+
+        return Individual(individualGenerate,
+                          self.__toDecimal(individualGenerate))
+
     # realizando mutação nos filhos
 
     def mutation(self, children: list) -> list:
         mutants = []
-        # e o sinal ?
+
         for child in children:
             identification = list(child.identification)
 
@@ -121,6 +106,7 @@ class Operations:
             identification[pos] = '0' if identification[pos] == '1' else '1'
 
             child.identification = ''.join(identification)
+            child.value = self.__toDecimal(child.identification)
 
             mutants.append(child)
 
