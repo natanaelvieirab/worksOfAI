5b8ef7202103af9c8ab24e44c4265b287f7317ec
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-11-13 15:18:10 -0300

    feat: adicionado metodo para calcular probabilidade e removido comentarios

--- Trabalho 02/Genetic.py
+++ Trabalho 02/Genetic.py
@@ -1,19 +1,25 @@
-# f(x) = x2 - 3x + 4 assume o valor mínimo.
-# [x] Assumir que x [-10, +10]
-# [x] Codificar X como vetor binário
-# [x] Criar uma população inicial com 4 indivíduos
-# [] Aplicar Mutação com taxa de 1%
-# [] Aplicar Crossover com taxa de 60%
-# [] Usar seleção por torneio.
-# [x] Usar 5 gerações.
-from utils.Individual import Individual
 from utils.Operations import Operations
-from heapq import heappush
+from heapq import heappop, heappush
 
 
 class Genetic(Operations):
     def __init__(self, restrictionsPopulation=[], generationLimit=5, sizePopulation=4):
         super().__init__(restrictionsPopulation, generationLimit, sizePopulation)
+
+    def calculatingProbability(self, heapFitnessFunction):
+        sum = 0
+        heapProbability = []
+
+        for individual in self.population:
+            result = super().fitnessFunction(individual.value)
+            sum += result
+            heappush(heapFitnessFunction, (result, individual.identification))
+
+        for h in heapFitnessFunction:
+            probability = (h[0]/sum) * 100
+            heappush(heapProbability, (probability, h[1]))
+
+        return heapProbability
 
     def start(self):
 
@@ -24,9 +30,7 @@ class Genetic(Operations):
             print(f"Geração {i+1}")
             heap = []
 
-            for individual in self.population:
-                result = super().fitnessFunction(individual.value)
-                heappush(heap, (result, individual.identification))
+            heap = self.calculatingProbability(heap)
 
             # escolher os melhores para o cross-over
             children = self.bestBoys(heap)
