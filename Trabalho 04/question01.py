d3e485f624cd86bd374484bc51a13a7447939272
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2022-02-07 21:11:03 -0300

    refactor: resolucao da questão 1

--- Trabalho 04/question01.py
+++ Trabalho 04/question01.py
@@ -1,20 +1,53 @@
-import nltk
 from nltk.corpus import gutenberg
-from nltk.text import Text
 
-# todos os arquivos
-#files = gutenberg.fileids()
+def main():
+    listText = ["shakespeare-caesar.txt", "shakespeare-hamlet.txt","shakespeare-macbeth.txt"]
 
-listText = ["shakespeare-caesar.txt", "shakespeare-hamlet.txt","shakespeare-macbeth.txt"]
+    for title in listText:
+        words = gutenberg.words(title)
+        sents = gutenberg.sents(title)
 
-for title in listText:
-    words = gutenberg.words(title)
-    sentences = gutenberg.sent_tokenize(title)
+        avg = len(words)/len(sents)
 
-    print(">> Referente à "+title+" <<")
-    print("Número total de palavras: {0}".format(len(words)))
-    print("Número total de sentenças : ", sentences)
-    # print("Número total de palavras não repetidas: ")
-    # print("Número total de palavras repetidas: ")
-    # print("Média de palavras por sentenças: ")
-    print("\n")
+        data = wordsInTheText(words)        
+
+        print(">> Referente à "+title+" <<")
+
+        print("Número total de palavras(COM REPETIÇÃO): {0}".format(len(words)))
+        print("Número total de sentenças : {0}".format(len(sents)))
+        print("Número total de palavras(SEM REPETIÇÃO): {0}".format(data['length']))
+        print("Número total de palavras não repetidas: {0} ".format(data['smaller']))
+        print("Número total de palavras repetidas: {0}".format(data['larger']))
+        print("Média de palavras por sentenças: {:.4f}".format(avg))
+
+        print("\n")
+
+def wordsInTheText( words):
+    i = 0
+    wordsDict = dict()
+
+    for word in words:
+        
+        value = wordsDict.get(word)
+        
+        if(value == None):
+            wordsDict[word] = 1
+        else:
+            wordsDict[word] += value
+
+    values = wordsDict.values()
+    
+    smaller = 0 #menor : número de repetições igual a 1 
+    larger = 0 #maior : número de repetições maior que 1 
+
+    for d in values:
+        if(d == 1):
+            smaller += 1
+        else:
+            larger += 1
+    
+    return {'length': len(values),'smaller' : smaller, 'larger': larger}
+
+
+
+main()
